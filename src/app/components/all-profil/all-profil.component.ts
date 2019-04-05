a1ff2ddec4d774513af49a8cb262b37a34c11b31
Maimouna Diallo <mayna.jallo@gmail.com>
2019-04-05 19:17:33 +0000

    image d'accueil téléchargée

---
+++ src/app/components/all-profil/all-profil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-profil',
+  templateUrl: './all-profil.component.html',
+  styleUrls: ['./all-profil.component.css']
+})
+export class AllProfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
