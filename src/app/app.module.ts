d65e7e2aaba493d70d0a2035a160d32c0723ed01
m.aita <aitambaye95@gmail.com>
2019-04-05 07:15:36 -0500

    formulaire d'inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionRecruteurComponent } from './components/inscription-recruteur/inscription-recruteur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionRecruteurComponent
   ],
   imports: [
     BrowserModule,
