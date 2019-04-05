d2b8893e1ae0df2bcf9ee5fcfdd7ed7daa52fa3d
falidevit <falliloudiaww@gmail.com>
2019-04-05 13:51:35 +0000

    Domaines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DomainesComponent } from './components/domaines/domaines.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import {Route} from '@angular/router'
+ const routes : Routes = [
+  {path:'',component:HomepageComponent},
+  {path: 'home',component:HomepageComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomainesComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
